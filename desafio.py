c742c896abbf71066cb60d7bf0c4b25d5affdd51
Matheus Rafael <78876451+math20122004@users.noreply.github.com>
2025-05-05 22:31:28 -0300

    Update desafio.py
    Commitando a 2ª versão do banco do Theuz.

--- desafio.py
+++ desafio.py
@@ -1,89 +1,212 @@
 import os
 import time
 
-def dados_conta():
-    return {
-        "saque_diario": 1,
-        "SAQUE_MAXIMO": 500.00,
-        "saldo": 0.0,
-        "extrato": []
-    }
+def verificar_cpf(*, clientes, cpf):
+    if cpf in clientes:
+        return True
+    return False
 
-def opcoes():
+def criar_conta(*, clientes, contas):
+    cpf =  str(input("Digite o CPF do titular: ")).strip()
+
+    if verificar_cpf(clientes=clientes, cpf=cpf) == False:
+        input("Cliente não encontrado.\nPor gentileza, cadastre o cliente para prosseguir.")
+        return contas
+    
+    AGENCIA = "0001"
+    
+    numero_conta = len(contas) + 1
+    conta_nova = {"saldo": 0, "AGENCIA": AGENCIA, "LIMITE_SAQUES": 3, "numeros saques": 0, "LIMITE": 500, "conta": numero_conta, "titular": clientes[cpf], "extrato": []}
+    contas.append(conta_nova)
+
+    clientes[cpf]["contas"].append(conta_nova)
+    
+    input(f"\nConta criada com sucesso!\nNúmero da conta: {numero_conta}\nPressione Enter para continuar.")
+    return contas
+
+def criar_cliente(clientes):
+    cpf = str(input("Digite o seu CPF: "))
+
+    if verificar_cpf(clientes=clientes, cpf=cpf):
+        input("\nUsuário já cadastrado.\nPressione a tecla ""Enter"" para continuar...")
+    else:
+        nome = str(input("Digite o seu nome: "))
+        data_nascimento = str(input("Digite a sua data de nascimento: "))
+        
+        logadouro = str(input("Digite o logradouro: "))
+        numero = str(input("Digite o número: "))
+        bairro = str(input("Digite o bairro: "))
+        cidade = str(input("Digite a sua cidade: "))
+
+        estados_validos = {
+        "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA",
+        "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+        "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+    }
+        while True:
+            estado = input("Digite a sigla do seu estado (ex: SP): ").strip().upper()
+
+            if estado in estados_validos:
+                break
+            else:
+                print("Sigla inválida! Tente novamente.")
+
+        
+        # endereco_completo = logadouro.rstrip() + " - " + numero.rstrip() + ", " + bairro.rstrip() + ". " + cidade.rstrip() + "/" + estado.strip()
+        clientes[cpf] = {"nome": nome, "data de nascimento": data_nascimento, "endereco completo": {"logradouro": logadouro, "bairro": bairro, "numero": numero, "cidade": cidade, "estado": estado}, "contas": []}
+    return clientes
+
+def mostrar_contas(clientes, cpf):
+    contas = clientes[cpf]["contas"]
+    
+    if not contas:
+        print("Nenhuma conta cadastrada para este cliente.")
+        return
+
+    print(f"\nContas do cliente {clientes[cpf]['nome']}:\n")
+    for conta in contas:
+        print(f"Agência: {conta['AGENCIA']} | Conta: {conta['conta']}")
+    
+    input("\nPressione ""ENTER"" para continuar...")
+
+
+def opcoes_conta():
     os.system("cls")
     print(f"""
 {"BANCO DO THEUZ".center(50, "=")}
 
-[1] Depósito
-[2] Saque
-[3] Extrato
-[4] Sair
+[0] Depósito
+[1] Saque
+[2] Extrato
+[3] Listar Contas
+[4] Voltar
 
 {"".center(50,"=")}
           """)
     
     opcao_escolhida = int(input())
 
-    if 1 <= opcao_escolhida <= 4 :
+    if 0 <= opcao_escolhida <= 4:
         os.system("cls")
         return opcao_escolhida
     
-    
-    return input("\nDigite uma opção válida.\nPrecione a tecla ""Enter"" para continuar")
+    return input("\nDigite uma opção válida.\nPressione a tecla ""Enter"" para continuar")
 
-def deposito(conta):
-    valor = float(input("\nInsira o valor que deseja depositar: "))
-    
-    if valor <= 0:
-        return input("\nNão foi possível realizar o depósito. Por favor insira um valor positivo.\nPrecione a tecla ""Enter"" para continuar")
-    
-    conta["extrato"].append(f"Depósito R$ {valor:.2f}")
-    conta["saldo"] += valor
-    return input(f"O depósito no valor de R$ {valor:.2f} foi realizado com sucesso!\nPrecione a tecla ""Enter"" para continuar")
-    
-def saque(conta):
-    valor = float(input("\nInsira o valor que deseja sacar: "))
+def opcoes_primaria():
+    os.system("cls")
+    print(f"""
+{"BANCO DO THEUZ".center(50, "=")}
 
-    if valor <= 0:
-        return input("\nNão foi possível realizar o saque. Por favor insira um valor positivo.\nPrecione a tecla ""Enter"" para continuar")
-    elif valor > conta["SAQUE_MAXIMO"]:
-        return input(f"\nNão foi possível realizar o saque. Você inseriu um valor maior que o limite de R$ {conta["SAQUE_MAXIMO"]:.2f} por saque.\nPrecione a tecla ""Enter"" para continuar")
-    elif valor > conta["saldo"]:
-        return input("\nNão foi possível realizar o saque por falta de saldo.\nPrecione a tecla ""Enter"" para continuar")
-    elif conta["saque_diario"] > 3:
-        return input("\nNão foi possível realizar o saque. Você atingiu o limite máximo de saque diário. Tente novamente amanhã!\nPrecione a tecla ""Enter"" para continuar")
+[0] Criar Cliente
+[1] Criar Conta
+[2] Entrar na Conta
+[3] Sair
+
+{"".center(50,"=")}
+          """)
     
-    conta["saldo"] -= valor
-    conta["extrato"].append(f"Saque R$ {valor:.2f}")
-    conta["saque_diario"] += 1
+    opcao_escolhida = int(input())
 
-    return input(f"O saque no valor de R$ {valor} foi realizado com sucesso!\nPrecione a tecla ""Enter"" para continuar")
+    if 0 <= opcao_escolhida <= 3 :
+        os.system("cls")
+        return opcao_escolhida
+    
+    return input("\nDigite uma opção válida.\nPressione a tecla ""Enter"" para continuar")
 
-def extrato(conta):
-    for dado in conta["extrato"]:
-        print(dado)
-    input(f"\nSaldo atual da conta: R$ {conta["saldo"]:.2f}.\nPrecione a tecla ""Enter"" para continuar")
 
-def sistema_bancario():
-    conta = dados_conta()
+def escolher_contas(cpf, clientes):
+    contas = clientes[cpf]["contas"]
+
+    for conta in contas:
+        print(f"Agência: {conta['AGENCIA']} | Conta: {conta['conta']} | Saldo: {conta['saldo']}")
+
+    conta_deposito = input("Digite o número da conta em que deseja realizar a operação: ").strip()
+    conta_selecionada = None
+
+    for conta in contas:
+        if str(conta["conta"]) == conta_deposito:
+            conta_selecionada = conta
+            break
+
+    if not conta_selecionada:
+        input("Número de conta não encontrado para este CPF. Pressione ENTER para continuar...")
+        return
+        
+    return conta_selecionada
+    
+    
+
+def sacar(*, conta_selecionada):
+    while True:
+        valor = float(input("\nInsira o valor que deseja sacar: "))
+
+        if valor <= 0:
+            input("\nNão foi possível realizar o saque. Por favor insira um valor positivo.\nPressione a tecla ""Enter"" para continuar")
+        elif valor > conta_selecionada["LIMITE"]:
+            input(f"\nNão foi possível realizar o saque. Você inseriu um valor maior que o limite de R$ {conta_selecionada["LIMITE"]:.2f} por saque.\nPressione a tecla ""Enter"" para continuar")
+        elif valor > conta_selecionada["saldo"]:
+            input("\nNão foi possível realizar o saque por falta de saldo.\nPressione a tecla ""Enter"" para continuar")
+        elif conta_selecionada["numeros saques"] >= conta_selecionada["LIMITE_SAQUES"]:
+            input("\nNão foi possível realizar o saque. Você atingiu o limite máximo de saque diário. Tente novamente amanhã!\nPressione a tecla ""Enter"" para continuar")
+        else:
+            conta_selecionada["saldo"] -= valor
+            conta_selecionada["extrato"].append(f"Saque R$ {valor:.2f}")
+            input(f"O saque no valor de R$ {valor:.2f} foi realizado com sucesso!\nPressione a tecla ""Enter"" para continuar")
+            break
+
+def depositar(conta_selecionada, /):
+    while True:
+        valor = float(input("\nInsira o valor que deseja depositar: "))
+        
+        if valor <= 0:
+            input("\nNão foi possível realizar o depósito. Por favor insira um valor positivo.\nPressione a tecla ""Enter"" para continuar")
+        else:
+            conta_selecionada["saldo"] += valor
+            conta_selecionada["extrato"].append(f"Depósito R$ {valor:.2f}")
+            input(f"O depósito no valor de R$ {valor:.2f} foi realizado com sucesso!\nPressione a tecla ""Enter"" para continuar")
+            break
+
+def extrato_conta(conta_selecionada):
+    for extrato in conta_selecionada["extrato"]:
+        print(extrato)
+    input(f"\nSaldo atual da conta: R$ {conta_selecionada["saldo"]}.\nPressione a tecla ""Enter"" para continuar")
+
+def main():
+    extrato = []
+    clientes = {}
+    contas = []
 
     while True:
-        opcao = opcoes()
+        opcao = opcoes_primaria()
 
+        if opcao == 0:
+            clientes = criar_cliente(clientes)
         if opcao == 1:
-            deposito(conta)
+            contas = criar_conta(clientes=clientes, contas=contas)
         if opcao == 2:
-            saque(conta)
+            cpf = str(input("Digite o CPF do titular: "))
+            if verificar_cpf(clientes=clientes, cpf=cpf) == False:
+                print("CPF não encontrado.\nCadastre um usuário")
+                time.sleep(3)
+            else:
+                conta_selecionada = escolher_contas(cpf, clientes)
+                while True:   
+                    opcao_dois = opcoes_conta()
+
+                    if opcao_dois == 0:
+                        depositar(conta_selecionada)
+                    if opcao_dois == 1:
+                        sacar(conta_selecionada=conta_selecionada)
+                    if opcao_dois == 2:
+                        extrato_conta(conta_selecionada)
+                    if opcao_dois == 3: 
+                        mostrar_contas(clientes, cpf)
+                    if opcao_dois == 4:
+                        break
         if opcao == 3:
-            extrato(conta)
-        if opcao == 4:
             print("Obrigado!\nVolte Sempre.")
             time.sleep(3)
             break
-            
-
-def main():
-    sistema_bancario()
     
 if __name__ == "__main__":
     main()
